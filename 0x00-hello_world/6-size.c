91398abcc80fe518e757001c0cc9425a9e5abcc8
klyceworld <kayboysays@gmail.com>
2023-02-16 13:29:19 -0800

    compiled and printed size of some data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char: %d\nbyte(s)", sizeof(a));
-printf("Size of a int: %d\nbyte(s)", sizeof(b));
-printf("Size of long int: %d\nbyte(s)", sizeof(c));
-printf("Size of long long int: %d\nbyte(s)", sizeof(d));
-printf("Size of float: %d\nbyte(s)", sizeof(f));
+printf("Size of a char: %lu byte(s)\n",(unsigned long) sizeof(a));
+printf("Size of a int: %lu byte(s)\n",(unsigned long) sizeof(b));
+printf("Size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
+}
