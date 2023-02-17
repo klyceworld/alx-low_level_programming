2258b66e35ac04af6ab2b3438d429e32f97b1d32
klyceworld <kayboysays@gmail.com>
2023-02-17 14:07:44 -0800

    code that prints a-z and A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *  main - A program that prints a-z and A-Z
+ *  Return: 0 (success)
+ */
+int main(void)
+{
+	char y;
+	char z;
+
+	y = ('a');
+	while (y <= 'z')
+	{ putchar(y);
+		y++;
+	}
+	z = ('A');
+	while (z <= 'Z')
+	{ putchar(z);
+		z++;
+	}
+
+	return (0);
+}
