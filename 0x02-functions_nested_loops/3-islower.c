75ea04cf327c48a69a4845c831b4cbb6f5619033
klyceworld <kayboysays@gmail.com>
2023-02-21 15:16:13 -0800

    checked for lower case alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _islower - check for lower case alphabet
- * 'c' - is assigned to check the balance
+ * c - is assigned to check the balance
  * Return: 1 (success) 0 (false)
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c >= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
